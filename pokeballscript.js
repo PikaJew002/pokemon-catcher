73b3b4587331c2fe66968100d0165cae7f55603b
PikaJew002 <aaron@ironmthome.com>
2020-09-27 21:31:08 -0400

    Update pokeballscript.js

--- pokeballscript.js
+++ pokeballscript.js
@@ -1,3 +1,11 @@
+// Aaron! You have nice looking code. 
+// Here are some suggestions that would as least make it slightly more performant/are good practice
+// The stacked if statements (as is) would all be tested even if the first or second test true. 
+// I would suggest returning early (add a return; inside the first two ifs, at the bottom after the code (see below))
+// Alternatively, you could use an if/if-else/else chain to accomplish the same thing. Or a switch statement.
+// Anyway you choose the idea is to not evaluate any expressions you don't need to. 
+// Props on the '===', btw, most people opt for the less strict version (which can cause confusion/problems later).
+// Cheers!
 let ballThrow = (ball) => {
     let number = (Math.floor(Math.random() * 10) + 1);
     let returnArea = document.querySelector("#resultDiv");
@@ -7,6 +15,7 @@ let ballThrow = (ball) => {
         } else {
             returnArea.innerHTML += "<p>Pokemon escaped.</p>";
         }
+// return;
     }
 
     if (ball === "greatball") {
@@ -15,6 +24,7 @@ let ballThrow = (ball) => {
         } else {
             returnArea.innerHTML += "<p>Pokemon escaped.</p>";
         }
+// return;
     }
 
     if (ball === "ultraball") {
@@ -23,6 +33,7 @@ let ballThrow = (ball) => {
         } else {
             returnArea.innerHTML += "<p>Pokemon escaped.</p>";
         }
+// return;
     }
 
     if (ball === "masterball") {
